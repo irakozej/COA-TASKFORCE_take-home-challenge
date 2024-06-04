4fd716eaa1b55c98420f658615bc59aac7121c0b
irakozej <jeaniakoze68@gmail.com>
2024-06-04 23:05:58 +0200

    committed the foundation of the project, will work on the interaction in the next phase.

---
+++ Interactive Photo Gallery/javascriptfiles/index.js
@@ -0,0 +1,9 @@
+function showTitle(card) {
+  const title = card.querySelector("hiddentext");
+  title.style.display = "block";
+}
+
+function hideTitle(card) {
+  const title = card.querySelector("hiddentext");
+  title.style.display = "none";
+}
